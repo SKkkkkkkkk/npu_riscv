330cacde6ef9e7e4e87b417810f03c645199291c
sk <pi3l4l5q2b@gmail.com>
2025-03-17 16:28:07 +0800

    ucaller

--- app/hello/build.sh
+++ app/hello/build.sh
@@ -1,3 +1,8 @@
 # CC=riscv32-unknown-elf-gcc cmake -S ../.. -B build -DPROJECT_NAME=hello -DPROJECT_PATH=. -DCMAKE_BUILD_TYPE=Release
-CC=riscv32-unknown-elf-gcc cmake -S ../.. -B build -DPROJECT_NAME=hello -DEXTRA_SRCS="src/main.c;src/a.c"
+CC=riscv32-unknown-elf-gcc \
+cmake -S ../.. -B build \
+-G Ninja \
+-DPROJECT_NAME=hello \
+-DPROJECT_PATH="." \
+-DCMAKE_BUILD_TYPE=Release
 cmake --build build
--- app/hello/src/main.c
+++ app/hello/src/main.c
@@ -1,1 +1,39 @@
-int main(){}
+#include "universal_caller.h"
+#include <stddef.h>
+
+#if 0
+/* Test mixing different scalar argument types */
+float test_mixed_types(char a1, short a2, int a3, long long a4, float a5,
+                        double a6, void *a7) {
+  return (int8_t)a1 + a2 + a3 + a4 + a5 + a6 + (intptr_t)a7;
+}
+
+
+static func_t test_mixed_types_func = {
+  .func = test_mixed_types,
+  .ret_type = RET_FLOAT,
+  .arg_count = 7,
+  .args = (arg_t[]){{ARG_CHAR, {.c = -1}}, {ARG_SHORT, {.s = -2}},
+                    {ARG_INT, {.i = 30000}},
+                    {ARG_LONG_LONG, {.ll = 400000LL}},
+                    {ARG_FLOAT, {.f = -5.5f}},
+                    {ARG_DOUBLE, {.d = 6.6}},
+                    {ARG_POINTER, {.p = (void *)7}}}
+};
+
+int main() {
+  return_value_t res;
+  *(func_t**)(0xf0300000 + 0x160) = &test_mixed_types_func;
+  res = universal_caller(*(func_t**)(0xf0300000 + 0x160));
+  *(float*)(0xf0300000 + 0x164) = res.f;
+  return 0;
+}
+#else
+
+int main() {
+  (void)universal_caller(*(func_t**)(0xf0300000 + 0x160));
+  __builtin_unreachable();
+  return 0;
+}
+
+#endif
